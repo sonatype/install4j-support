1d21c12e4960fa0c52242b7d3a2ac4c01b7d4b69
Jason Dillon <jason@planet57.com>
2012-08-17 12:37:42 -0700

    Wagon: Deploying site to repository

---
+++ install4j-maven-plugin/apidocs/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "default package", "default package/package-summary.html", [ [ "Classes", null, [ [ "HelpMojo", "default package/HelpMojo.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.install4j.maven", "org/sonatype/install4j/maven/package-summary.html", [ [ "Classes", null, [ [ "AntHelper", "org/sonatype/install4j/maven/AntHelper.html", null, "" ], [ "CompileMojo", "org/sonatype/install4j/maven/CompileMojo.html", null, "" ], [ "Install4jcMojoSupport", "org/sonatype/install4j/maven/Install4jcMojoSupport.html", null, "" ], [ "InstallLicenseMojo", "org/sonatype/install4j/maven/InstallLicenseMojo.html", null, "" ], [ "MavenAntLoggerAdapter", "org/sonatype/install4j/maven/MavenAntLoggerAdapter.html", null, "" ], [ "MojoSupport", "org/sonatype/install4j/maven/MojoSupport.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ install4j-maven-plugin/apidocs/reference/lists.js
@@ -0,0 +1,12 @@
+var DATA = [
+      { id:0, label:"HelpMojo", link:"default package/HelpMojo.html", type:"class" },
+      { id:1, label:"default package", link:"default package/package-summary.html", type:"package" },
+      { id:2, label:"org.sonatype.install4j.maven", link:"org/sonatype/install4j/maven/package-summary.html", type:"package" },
+      { id:3, label:"org.sonatype.install4j.maven.AntHelper", link:"org/sonatype/install4j/maven/AntHelper.html", type:"class" },
+      { id:4, label:"org.sonatype.install4j.maven.CompileMojo", link:"org/sonatype/install4j/maven/CompileMojo.html", type:"class" },
+      { id:5, label:"org.sonatype.install4j.maven.Install4jcMojoSupport", link:"org/sonatype/install4j/maven/Install4jcMojoSupport.html", type:"class" },
+      { id:6, label:"org.sonatype.install4j.maven.InstallLicenseMojo", link:"org/sonatype/install4j/maven/InstallLicenseMojo.html", type:"class" },
+      { id:7, label:"org.sonatype.install4j.maven.MavenAntLoggerAdapter", link:"org/sonatype/install4j/maven/MavenAntLoggerAdapter.html", type:"class" },
+      { id:8, label:"org.sonatype.install4j.maven.MojoSupport", link:"org/sonatype/install4j/maven/MojoSupport.html", type:"class" }
+
+    ];
