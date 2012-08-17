ba8a25abc690c6ab6f5ce8e6494c63e3c07c618b
Jason Dillon <jason@planet57.com>
2012-08-17 12:35:08 -0700

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,15 @@
+var NAVTREE_DATA =
+[ [ "default package", "default package/package-summary.html", [ [ "Classes", null, [ [ "HelpMojo", "default package/HelpMojo.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.slf4j.impl", "org/slf4j/impl/package-summary.html", [ [ "Classes", null, [ [ "StaticLoggerBinder", "org/slf4j/impl/StaticLoggerBinder.html", null, "" ], [ "StaticMarkerBinder", "org/slf4j/impl/StaticMarkerBinder.html", null, "" ], [ "StaticMDCBinder", "org/slf4j/impl/StaticMDCBinder.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.install4j.ga", "org/sonatype/install4j/ga/package-summary.html", [ [ "Classes", null, [ [ "ActionBeanInfoSupport", "org/sonatype/install4j/ga/ActionBeanInfoSupport.html", null, "" ], [ "ActionSupport", "org/sonatype/install4j/ga/ActionSupport.html", null, "" ], [ "ConfigureProxyAction", "org/sonatype/install4j/ga/ConfigureProxyAction.html", null, "" ], [ "ConfigureProxyActionBeanInfo", "org/sonatype/install4j/ga/ConfigureProxyActionBeanInfo.html", null, "" ], [ "GoogleAnalyticsActionSupport", "org/sonatype/install4j/ga/GoogleAnalyticsActionSupport.html", null, "" ], [ "TrackerFactory", "org/sonatype/install4j/ga/TrackerFactory.html", null, "" ], [ "TrackEventAction", "org/sonatype/install4j/ga/TrackEventAction.html", null, "" ], [ "TrackEventActionBeanInfo", "org/sonatype/install4j/ga/TrackEventActionBeanInfo.html", null, "" ], [ "TrackEventOnCancelAction", "org/sonatype/install4j/ga/TrackEventOnCancelAction.html", null, "" ], [ "TrackEventOnCancelActionBeanInfo", "org/sonatype/install4j/ga/TrackEventOnCancelActionBeanInfo.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.install4j.maven", "org/sonatype/install4j/maven/package-summary.html", [ [ "Classes", null, [ [ "AntHelper", "org/sonatype/install4j/maven/AntHelper.html", null, "" ], [ "CompileMojo", "org/sonatype/install4j/maven/CompileMojo.html", null, "" ], [ "Install4jcMojoSupport", "org/sonatype/install4j/maven/Install4jcMojoSupport.html", null, "" ], [ "InstallLicenseMojo", "org/sonatype/install4j/maven/InstallLicenseMojo.html", null, "" ], [ "MavenAntLoggerAdapter", "org/sonatype/install4j/maven/MavenAntLoggerAdapter.html", null, "" ], [ "MojoSupport", "org/sonatype/install4j/maven/MojoSupport.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.install4j.slf4j", "org/sonatype/install4j/slf4j/package-summary.html", [ [ "Classes", null, [ [ "Install4jLogger", "org/sonatype/install4j/slf4j/Install4jLogger.html", null, "" ], [ "Install4jLoggerFactory", "org/sonatype/install4j/slf4j/Install4jLoggerFactory.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
