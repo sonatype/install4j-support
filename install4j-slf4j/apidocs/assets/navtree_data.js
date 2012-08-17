a7bce5aa4f50a6eaeb40ad51f48279a1e09d0a39
Jason Dillon <jason@planet57.com>
2012-08-17 12:35:56 -0700

    Wagon: Deploying site to repository

---
+++ install4j-slf4j/apidocs/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "org.slf4j.impl", "org/slf4j/impl/package-summary.html", [ [ "Classes", null, [ [ "StaticLoggerBinder", "org/slf4j/impl/StaticLoggerBinder.html", null, "" ], [ "StaticMarkerBinder", "org/slf4j/impl/StaticMarkerBinder.html", null, "" ], [ "StaticMDCBinder", "org/slf4j/impl/StaticMDCBinder.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.sonatype.install4j.slf4j", "org/sonatype/install4j/slf4j/package-summary.html", [ [ "Classes", null, [ [ "Install4jLogger", "org/sonatype/install4j/slf4j/Install4jLogger.html", null, "" ], [ "Install4jLoggerFactory", "org/sonatype/install4j/slf4j/Install4jLoggerFactory.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ install4j-slf4j/apidocs/reference/lists.js
@@ -0,0 +1,10 @@
+var DATA = [
+      { id:0, label:"org.slf4j.impl", link:"org/slf4j/impl/package-summary.html", type:"package" },
+      { id:1, label:"org.slf4j.impl.StaticLoggerBinder", link:"org/slf4j/impl/StaticLoggerBinder.html", type:"class" },
+      { id:2, label:"org.slf4j.impl.StaticMDCBinder", link:"org/slf4j/impl/StaticMDCBinder.html", type:"class" },
+      { id:3, label:"org.slf4j.impl.StaticMarkerBinder", link:"org/slf4j/impl/StaticMarkerBinder.html", type:"class" },
+      { id:4, label:"org.sonatype.install4j.slf4j", link:"org/sonatype/install4j/slf4j/package-summary.html", type:"package" },
+      { id:5, label:"org.sonatype.install4j.slf4j.Install4jLogger", link:"org/sonatype/install4j/slf4j/Install4jLogger.html", type:"class" },
+      { id:6, label:"org.sonatype.install4j.slf4j.Install4jLoggerFactory", link:"org/sonatype/install4j/slf4j/Install4jLoggerFactory.html", type:"class" }
+
+    ];
