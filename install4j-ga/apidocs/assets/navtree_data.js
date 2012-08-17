ac2171d84e0276b1aab270a501b8a21d06d9ab68
Jason Dillon <jason@planet57.com>
2012-08-17 12:36:44 -0700

    Wagon: Deploying site to repository

---
+++ install4j-ga/apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.sonatype.install4j.ga", "org/sonatype/install4j/ga/package-summary.html", [ [ "Classes", null, [ [ "ActionBeanInfoSupport", "org/sonatype/install4j/ga/ActionBeanInfoSupport.html", null, "" ], [ "ActionSupport", "org/sonatype/install4j/ga/ActionSupport.html", null, "" ], [ "ConfigureProxyAction", "org/sonatype/install4j/ga/ConfigureProxyAction.html", null, "" ], [ "ConfigureProxyActionBeanInfo", "org/sonatype/install4j/ga/ConfigureProxyActionBeanInfo.html", null, "" ], [ "GoogleAnalyticsActionSupport", "org/sonatype/install4j/ga/GoogleAnalyticsActionSupport.html", null, "" ], [ "TrackerFactory", "org/sonatype/install4j/ga/TrackerFactory.html", null, "" ], [ "TrackEventAction", "org/sonatype/install4j/ga/TrackEventAction.html", null, "" ], [ "TrackEventActionBeanInfo", "org/sonatype/install4j/ga/TrackEventActionBeanInfo.html", null, "" ], [ "TrackEventOnCancelAction", "org/sonatype/install4j/ga/TrackEventOnCancelAction.html", null, "" ], [ "TrackEventOnCancelActionBeanInfo", "org/sonatype/install4j/ga/TrackEventOnCancelActionBeanInfo.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ install4j-ga/apidocs/reference/lists.js
@@ -0,0 +1,14 @@
+var DATA = [
+      { id:0, label:"org.sonatype.install4j.ga", link:"org/sonatype/install4j/ga/package-summary.html", type:"package" },
+      { id:1, label:"org.sonatype.install4j.ga.ActionBeanInfoSupport", link:"org/sonatype/install4j/ga/ActionBeanInfoSupport.html", type:"class" },
+      { id:2, label:"org.sonatype.install4j.ga.ActionSupport", link:"org/sonatype/install4j/ga/ActionSupport.html", type:"class" },
+      { id:3, label:"org.sonatype.install4j.ga.ConfigureProxyAction", link:"org/sonatype/install4j/ga/ConfigureProxyAction.html", type:"class" },
+      { id:4, label:"org.sonatype.install4j.ga.ConfigureProxyActionBeanInfo", link:"org/sonatype/install4j/ga/ConfigureProxyActionBeanInfo.html", type:"class" },
+      { id:5, label:"org.sonatype.install4j.ga.GoogleAnalyticsActionSupport", link:"org/sonatype/install4j/ga/GoogleAnalyticsActionSupport.html", type:"class" },
+      { id:6, label:"org.sonatype.install4j.ga.TrackEventAction", link:"org/sonatype/install4j/ga/TrackEventAction.html", type:"class" },
+      { id:7, label:"org.sonatype.install4j.ga.TrackEventActionBeanInfo", link:"org/sonatype/install4j/ga/TrackEventActionBeanInfo.html", type:"class" },
+      { id:8, label:"org.sonatype.install4j.ga.TrackEventOnCancelAction", link:"org/sonatype/install4j/ga/TrackEventOnCancelAction.html", type:"class" },
+      { id:9, label:"org.sonatype.install4j.ga.TrackEventOnCancelActionBeanInfo", link:"org/sonatype/install4j/ga/TrackEventOnCancelActionBeanInfo.html", type:"class" },
+      { id:10, label:"org.sonatype.install4j.ga.TrackerFactory", link:"org/sonatype/install4j/ga/TrackerFactory.html", type:"class" }
+
+    ];
