fc7afd6c8608dc20f850183a379627a1ecff7b04
Jason Dillon <jason@planet57.com>
2012-12-06 13:45:09 -0800

    Wagon: Deploying site to repository

--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -1,30 +1,5 @@
 var DATA = [
-      { id:0, label:"HelpMojo", link:"default package/HelpMojo.html", type:"class" },
-      { id:1, label:"default package", link:"default package/package-summary.html", type:"package" },
-      { id:2, label:"org.slf4j.impl", link:"org/slf4j/impl/package-summary.html", type:"package" },
-      { id:3, label:"org.slf4j.impl.StaticLoggerBinder", link:"org/slf4j/impl/StaticLoggerBinder.html", type:"class" },
-      { id:4, label:"org.slf4j.impl.StaticMDCBinder", link:"org/slf4j/impl/StaticMDCBinder.html", type:"class" },
-      { id:5, label:"org.slf4j.impl.StaticMarkerBinder", link:"org/slf4j/impl/StaticMarkerBinder.html", type:"class" },
-      { id:6, label:"org.sonatype.install4j.ga", link:"org/sonatype/install4j/ga/package-summary.html", type:"package" },
-      { id:7, label:"org.sonatype.install4j.ga.ActionBeanInfoSupport", link:"org/sonatype/install4j/ga/ActionBeanInfoSupport.html", type:"class" },
-      { id:8, label:"org.sonatype.install4j.ga.ActionSupport", link:"org/sonatype/install4j/ga/ActionSupport.html", type:"class" },
-      { id:9, label:"org.sonatype.install4j.ga.ConfigureProxyAction", link:"org/sonatype/install4j/ga/ConfigureProxyAction.html", type:"class" },
-      { id:10, label:"org.sonatype.install4j.ga.ConfigureProxyActionBeanInfo", link:"org/sonatype/install4j/ga/ConfigureProxyActionBeanInfo.html", type:"class" },
-      { id:11, label:"org.sonatype.install4j.ga.GoogleAnalyticsActionSupport", link:"org/sonatype/install4j/ga/GoogleAnalyticsActionSupport.html", type:"class" },
-      { id:12, label:"org.sonatype.install4j.ga.TrackEventAction", link:"org/sonatype/install4j/ga/TrackEventAction.html", type:"class" },
-      { id:13, label:"org.sonatype.install4j.ga.TrackEventActionBeanInfo", link:"org/sonatype/install4j/ga/TrackEventActionBeanInfo.html", type:"class" },
-      { id:14, label:"org.sonatype.install4j.ga.TrackEventOnCancelAction", link:"org/sonatype/install4j/ga/TrackEventOnCancelAction.html", type:"class" },
-      { id:15, label:"org.sonatype.install4j.ga.TrackEventOnCancelActionBeanInfo", link:"org/sonatype/install4j/ga/TrackEventOnCancelActionBeanInfo.html", type:"class" },
-      { id:16, label:"org.sonatype.install4j.ga.TrackerFactory", link:"org/sonatype/install4j/ga/TrackerFactory.html", type:"class" },
-      { id:17, label:"org.sonatype.install4j.maven", link:"org/sonatype/install4j/maven/package-summary.html", type:"package" },
-      { id:18, label:"org.sonatype.install4j.maven.AntHelper", link:"org/sonatype/install4j/maven/AntHelper.html", type:"class" },
-      { id:19, label:"org.sonatype.install4j.maven.CompileMojo", link:"org/sonatype/install4j/maven/CompileMojo.html", type:"class" },
-      { id:20, label:"org.sonatype.install4j.maven.Install4jcMojoSupport", link:"org/sonatype/install4j/maven/Install4jcMojoSupport.html", type:"class" },
-      { id:21, label:"org.sonatype.install4j.maven.InstallLicenseMojo", link:"org/sonatype/install4j/maven/InstallLicenseMojo.html", type:"class" },
-      { id:22, label:"org.sonatype.install4j.maven.MavenAntLoggerAdapter", link:"org/sonatype/install4j/maven/MavenAntLoggerAdapter.html", type:"class" },
-      { id:23, label:"org.sonatype.install4j.maven.MojoSupport", link:"org/sonatype/install4j/maven/MojoSupport.html", type:"class" },
-      { id:24, label:"org.sonatype.install4j.slf4j", link:"org/sonatype/install4j/slf4j/package-summary.html", type:"package" },
-      { id:25, label:"org.sonatype.install4j.slf4j.Install4jLogger", link:"org/sonatype/install4j/slf4j/Install4jLogger.html", type:"class" },
-      { id:26, label:"org.sonatype.install4j.slf4j.Install4jLoggerFactory", link:"org/sonatype/install4j/slf4j/Install4jLoggerFactory.html", type:"class" }
+      { id:0, label:"org.sonatype.install4j.maven", link:"org/sonatype/install4j/maven/package-summary.html", type:"package" },
+      { id:1, label:"org.sonatype.install4j.maven.VersionHelperTest", link:"org/sonatype/install4j/maven/VersionHelperTest.html", type:"class" }
 
     ];
